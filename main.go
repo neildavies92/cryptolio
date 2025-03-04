6355ef184a29aeedc05daa1d13d973bb3f08e123
Neil Davies <neil.davies92@gmail.com>
2025-03-04 19:46:41 +0000

    import quote package and call all functions

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
 }
