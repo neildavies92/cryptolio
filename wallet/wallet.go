e8c6fe975bc37e2098c38061edce6b33becd73da
Neil Davies <neil.davies92@gmail.com>
2025-03-07 15:06:06 +0000

    add error handling to http request

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -8,15 +8,20 @@ import (
 func GetWallet(jwt string) (*http.Response, error) {
 
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.coinbase.com/v2/accounts/", nil)
 
+	req, err := http.NewRequest("GET", "https://api.coinbase.com/v2/accounts/", nil)
+	req.Header.Add("Authorization", "Bearer "+jwt)
+	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return nil, err
 	}
-	req.Header.Add("Authorization", "Bearer "+jwt)
 
 	resp, err := client.Do(req)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: request failed with status: ", resp.Status)
+		return nil, err
+	}
 
 	if err != nil {
 		fmt.Printf("Error: %v", err)
